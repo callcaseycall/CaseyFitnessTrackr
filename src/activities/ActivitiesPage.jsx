2e90bf9e5133e63c7ad66b2eca00cbc89c94704a
callcaseycall <callcaseycall@gmail.com>
2025-08-12 20:38:07 -0600

    First commit, started on ActivitiesPage, list of activities showing up

--- src/activities/ActivitiesPage.jsx
+++ src/activities/ActivitiesPage.jsx
@@ -1,8 +1,18 @@
+import useQuery from "../api/useQuery";
+
 export default function ActivitiesPage() {
+  const { data } = useQuery("/activities");
+
+  console.log(data);
   return (
     <>
       <h1>Activities</h1>
       <p>Imagine all the activities!</p>
+      <ul>
+        {data?.map((element) => (
+          <li key={element.id}>{element.name}</li>
+        ))}
+      </ul>
     </>
   );
 }
